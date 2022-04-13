3dbd7b14e6108fdd3445a53b0d8cd48c7ea72690
thedasherx <75217270+rhit-webbma@users.noreply.github.com>
2022-04-13 13:08:52 -0400

    MethodLengthTest rewritten for Integration Tests
    MethodLengthTest rewritten for Integration Tests

--- src/test/java/domain/MethodLengthTest.java
+++ src/test/java/domain/MethodLengthTest.java
@@ -4,13 +4,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
 import org.easymock.EasyMock;
 import org.junit.jupiter.api.Test;
 
 import data_source.MyAbstractInsnNode;
 import data_source.MyClassNode;
+import data_source.MyFieldNode;
 import data_source.MyLineNumberNode;
+import data_source.MyLocalVariableNode;
 import data_source.MyMethodInsnNode;
 import data_source.MyMethodNode;
 import domain.MethodLengthCheck;
@@ -22,119 +25,274 @@ class MethodLengthTest {
 	@Test
 	public void testMethodEmpty()
 	{
-		MyClassNode currentClass = EasyMock.createMock(MyClassNode.class);
+//		
 		ArrayList<MyClassNode> classes = new ArrayList<>();
-		classes.add(currentClass);
-		
-		EasyMock.expect(currentClass.getCleanName()).andReturn("TestClass");
-		EasyMock.expect(currentClass.getCleanName()).andReturn("TestClass");
-		
-		MyMethodNode testMethod = EasyMock.createMock(MyMethodNode.class);
 		
 		LinkedList<MyAbstractInsnNode> instructionsList = new LinkedList<MyAbstractInsnNode>(); 
+		List<MyLocalVariableNode> localVars = new ArrayList<>();
 		
-		testMethod.name = "TestMethod";
 		
 //		for(int i = 0; i < 10; i++)
 //		{
-//			MyLineNumberNode insn = EasyMock.createMock(MyLineNumberNode.class);
-//			insn.line = i;
+////			MyLineNumberNode insn = EasyMock.createMock(MyLineNumberNode.class);
+////			insn.line = i;
+//			MyLineNumberNode insn = new MyLineNumberNode(i);
 //			MyAbstractInsnNode n1 = insn;
-//			instructionsList.add(n1);
+//			instructionsList.add(n1);	
 //		}
 		
-		testMethod.instructions = instructionsList;
 		
-		ArrayList<MyMethodNode> methods = new ArrayList<MyMethodNode>();
+		MyMethodNode testMethod = new MyMethodNode("TestMethod", "()V", instructionsList, localVars);
+		
+		List<String> interfaces = new ArrayList<>();
+		List<MyFieldNode> fields = new ArrayList<>();
+		List<MyMethodNode> methods = new ArrayList<>();
 		
 		methods.add(testMethod);
 		
-		currentClass.methods = methods;
+		MyClassNode currentClass = new MyClassNode("TestClass", "", interfaces, fields, methods, 0);
+		classes.add(currentClass);
 		
-		EasyMock.expect(testMethod.getLength()).andReturn(0);
 		
-		EasyMock.replay(currentClass, testMethod);
+		//Mock 
+//		EasyMock.expect(currentClass.getCleanName()).andReturn("TestClass");
+//		EasyMock.expect(currentClass.getCleanName()).andReturn("TestClass");
+//		MyClassNode currentClass = EasyMock.createMock(MyClassNode.class);
+		
+		
+//		MyMethodNode testMethod = EasyMock.createMock(MyMethodNode.class);
+//		testMethod.name = "TestMethod";
+		
+
+		
+//		testMethod.instructions = instructionsList;
+		
+//		ArrayList<MyMethodNode> methods = new ArrayList<MyMethodNode>();
+		
+//		currentClass.methods = methods;
 		
 		//Given that this method is under length, method length should return nothing
 		String expected = "";
 		
 		assertEquals(expected, checker.runCheck(classes));
 		
-		EasyMock.verify(currentClass, testMethod);
+//		
+//		EasyMock.expect(testMethod.getLength()).andReturn(0);
+//		
+//		EasyMock.replay(currentClass, testMethod);
+//		
+//		
+//		
+//		assertEquals(expected, checker.runCheck(classes));
+//		
+//		EasyMock.verify(currentClass, testMethod);
 		
 	}
 	
 	@Test
 	public void testMethodUnderLength()
 	{
-		MyClassNode currentClass = EasyMock.createMock(MyClassNode.class);
+//		
 		ArrayList<MyClassNode> classes = new ArrayList<>();
-		classes.add(currentClass);
-		
-		EasyMock.expect(currentClass.getCleanName()).andReturn("TestClass");
-		EasyMock.expect(currentClass.getCleanName()).andReturn("TestClass");
-		
-		MyMethodNode testMethod = EasyMock.createMock(MyMethodNode.class);
 		
 		LinkedList<MyAbstractInsnNode> instructionsList = new LinkedList<MyAbstractInsnNode>(); 
+		List<MyLocalVariableNode> localVars = new ArrayList<>();
 		
-		testMethod.name = "TestMethod";
 		
-		testMethod.instructions = instructionsList;
+		for(int i = 0; i < 10; i++)
+		{
+//			MyLineNumberNode insn = EasyMock.createMock(MyLineNumberNode.class);
+//			insn.line = i;
+			MyLineNumberNode insn = new MyLineNumberNode(i);
+			MyAbstractInsnNode n1 = insn;
+			instructionsList.add(n1);	
+		}
 		
-		ArrayList<MyMethodNode> methods = new ArrayList<MyMethodNode>();
+		
+		MyMethodNode testMethod = new MyMethodNode("TestMethod", "()V", instructionsList, localVars);
+		
+		List<String> interfaces = new ArrayList<>();
+		List<MyFieldNode> fields = new ArrayList<>();
+		List<MyMethodNode> methods = new ArrayList<>();
 		
 		methods.add(testMethod);
 		
-		currentClass.methods = methods;
+		MyClassNode currentClass = new MyClassNode("TestClass", "", interfaces, fields, methods, 0);
+		classes.add(currentClass);
 		
-		EasyMock.expect(testMethod.getLength()).andReturn(13);
 		
-		EasyMock.replay(currentClass, testMethod);
+		//Mock 
+//		EasyMock.expect(currentClass.getCleanName()).andReturn("TestClass");
+//		EasyMock.expect(currentClass.getCleanName()).andReturn("TestClass");
+//		MyClassNode currentClass = EasyMock.createMock(MyClassNode.class);
+		
+		
+//		MyMethodNode testMethod = EasyMock.createMock(MyMethodNode.class);
+//		testMethod.name = "TestMethod";
+		
+
+		
+//		testMethod.instructions = instructionsList;
+		
+//		ArrayList<MyMethodNode> methods = new ArrayList<MyMethodNode>();
+		
+//		currentClass.methods = methods;
 		
 		//Given that this method is under length, method length should return nothing
 		String expected = "";
 		
 		assertEquals(expected, checker.runCheck(classes));
 		
-		EasyMock.verify(currentClass, testMethod);
-		
+//		
+//		EasyMock.expect(testMethod.getLength()).andReturn(0);
+//		
+//		EasyMock.replay(currentClass, testMethod);
+//		
+//		
+//		
+//		assertEquals(expected, checker.runCheck(classes));
+//		
+//		EasyMock.verify(currentClass, testMethod);
 	}
+	
+//	@Test
+//	public void testMethodUnderLength()
+//	{
+//		MyClassNode currentClass = EasyMock.createMock(MyClassNode.class);
+//		ArrayList<MyClassNode> classes = new ArrayList<>();
+//		classes.add(currentClass);
+//		
+//		EasyMock.expect(currentClass.getCleanName()).andReturn("TestClass");
+//		EasyMock.expect(currentClass.getCleanName()).andReturn("TestClass");
+//		
+//		MyMethodNode testMethod = EasyMock.createMock(MyMethodNode.class);
+//		
+//		LinkedList<MyAbstractInsnNode> instructionsList = new LinkedList<MyAbstractInsnNode>(); 
+//		
+//		testMethod.name = "TestMethod";
+//		
+//		testMethod.instructions = instructionsList;
+//		
+//		ArrayList<MyMethodNode> methods = new ArrayList<MyMethodNode>();
+//		
+//		methods.add(testMethod);
+//		
+//		currentClass.methods = methods;
+//		
+//		EasyMock.expect(testMethod.getLength()).andReturn(13);
+//		
+//		EasyMock.replay(currentClass, testMethod);
+//		
+//		//Given that this method is under length, method length should return nothing
+//		String expected = "";
+//		
+//		assertEquals(expected, checker.runCheck(classes));
+//		
+//		EasyMock.verify(currentClass, testMethod);
+//		
+//	}
 	
 	@Test
 	public void testMethodOver35()
 	{
-		MyClassNode currentClass = EasyMock.createMock(MyClassNode.class);
+//		
 		ArrayList<MyClassNode> classes = new ArrayList<>();
-		classes.add(currentClass);
-		
-		EasyMock.expect(currentClass.getCleanName()).andReturn("TestClass");
-		EasyMock.expect(currentClass.getCleanName()).andReturn("TestClass");
-		
-		MyMethodNode testMethod = EasyMock.createMock(MyMethodNode.class);
 		
 		LinkedList<MyAbstractInsnNode> instructionsList = new LinkedList<MyAbstractInsnNode>(); 
+		List<MyLocalVariableNode> localVars = new ArrayList<>();
 		
-		testMethod.name = "TestMethod";
+		
+		for(int i = 0; i < 50; i++)
+		{
+//			MyLineNumberNode insn = EasyMock.createMock(MyLineNumberNode.class);
+//			insn.line = i;
+			MyLineNumberNode insn = new MyLineNumberNode(i);
+			MyAbstractInsnNode n1 = insn;
+			instructionsList.add(n1);	
+		}
 		
 		
-		testMethod.instructions = instructionsList;
+		MyMethodNode testMethod = new MyMethodNode("TestMethod", "()V", instructionsList, localVars);
 		
-		ArrayList<MyMethodNode> methods = new ArrayList<MyMethodNode>();
+		List<String> interfaces = new ArrayList<>();
+		List<MyFieldNode> fields = new ArrayList<>();
+		List<MyMethodNode> methods = new ArrayList<>();
 		
 		methods.add(testMethod);
 		
-		currentClass.methods = methods;
+		MyClassNode currentClass = new MyClassNode("TestClass", "", interfaces, fields, methods, 0);
+		classes.add(currentClass);
+		
+		
+		//Mock 
+//		EasyMock.expect(currentClass.getCleanName()).andReturn("TestClass");
+//		EasyMock.expect(currentClass.getCleanName()).andReturn("TestClass");
+//		MyClassNode currentClass = EasyMock.createMock(MyClassNode.class);
+		
+		
+//		MyMethodNode testMethod = EasyMock.createMock(MyMethodNode.class);
+//		testMethod.name = "TestMethod";
+		
 
-		EasyMock.expect(testMethod.getLength()).andReturn(40);
 		
-		EasyMock.replay(currentClass, testMethod);
+//		testMethod.instructions = instructionsList;
 		
+//		ArrayList<MyMethodNode> methods = new ArrayList<MyMethodNode>();
+		
+//		currentClass.methods = methods;
+		
+		//Given that this method is under length, method length should return nothing
 		String expected = "Method Length Check:\n	Class: TestClass\n" + "		Method: " + "TestMethod" + "\n";
-		expected += "			Method too long: (40 lines) Shorten it to 35 lines or less. \n";
+		expected += "			Method too long: (50 lines) Shorten it to 35 lines or less. \n";
 		
-		assertEquals(expected, checker.runCheck(classes));;
+		assertEquals(expected, checker.runCheck(classes));
 		
+//		
+//		EasyMock.expect(testMethod.getLength()).andReturn(0);
+//		
+//		EasyMock.replay(currentClass, testMethod);
+//		
+//		
+//		
+//		assertEquals(expected, checker.runCheck(classes));
+//		
+//		EasyMock.verify(currentClass, testMethod);
 	}
+	
+//	@Test
+//	public void testMethodOver35()
+//	{
+//		MyClassNode currentClass = EasyMock.createMock(MyClassNode.class);
+//		ArrayList<MyClassNode> classes = new ArrayList<>();
+//		classes.add(currentClass);
+//		
+//		EasyMock.expect(currentClass.getCleanName()).andReturn("TestClass");
+//		EasyMock.expect(currentClass.getCleanName()).andReturn("TestClass");
+//		
+//		MyMethodNode testMethod = EasyMock.createMock(MyMethodNode.class);
+//		
+//		LinkedList<MyAbstractInsnNode> instructionsList = new LinkedList<MyAbstractInsnNode>(); 
+//		
+//		testMethod.name = "TestMethod";
+//		
+//		
+//		testMethod.instructions = instructionsList;
+//		
+//		ArrayList<MyMethodNode> methods = new ArrayList<MyMethodNode>();
+//		
+//		methods.add(testMethod);
+//		
+//		currentClass.methods = methods;
+//
+//		EasyMock.expect(testMethod.getLength()).andReturn(40);
+//		
+//		EasyMock.replay(currentClass, testMethod);
+//		
+//		String expected = "Method Length Check:\n	Class: TestClass\n" + "		Method: " + "TestMethod" + "\n";
+//		expected += "			Method too long: (40 lines) Shorten it to 35 lines or less. \n";
+//		
+//		assertEquals(expected, checker.runCheck(classes));;
+//		
+//	}
 
 }
